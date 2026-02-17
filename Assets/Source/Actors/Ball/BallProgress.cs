87de902a27608a5b7e7c916bf735d200b2afa39a
highrower <jairojared@gmail.com>
2026-02-17 11:03:42 -0600

    renamed variables to better reflect that the assets represent GameData. also made ballprogress a SO and created the asset and put it  where it needs to go

--- Assets/Source/Actors/Ball/BallProgress.cs
+++ Assets/Source/Actors/Ball/BallProgress.cs
@@ -1,13 +1,21 @@
 ﻿// This is where data that is affected by upgrades lives. makes sense to have it be a normal class
 // because this shouldnt be modifyable in a serialized way since
 // it will only read from various upgrades/level shopupgrades
-public class BallProgress: ISaveable
-{
-    public string ID =>  "Ball Progress";
-    public int DamageLevel;
-    
-    public void Save(SaveData data) => data.UpgradeIdToLevel[ID] = DamageLevel;
+using UnityEngine;
 
-    public void Load(SaveData data) => data.UpgradeIdToLevel.TryGetValue(ID, out DamageLevel);
+[CreateAssetMenu(menuName = "Progress/Ball Progress")]
+public class BallProgress: ScriptableObject, ISaveable
+{
+    public string ID =>  "ball_progress";
+    
+    [Header("Progress")]
+    public int damageLevel;
+    
+    [Header("Defaults")]
+    public int defaultDamageLevel;
+    
+    public void Save(SaveData data) => data.UpgradeIdToLevel[ID] = damageLevel;
+
+    public void Load(SaveData data) => data.UpgradeIdToLevel.TryGetValue(ID, out damageLevel);
     
 }
